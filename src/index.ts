9680ddea7815e35eb58e71e3ab4641bc4875bafd
ale_figure8 <gomeznieto@gmail.com>
2021-11-29 20:44:11 -0300

    Adding routes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express from 'express'
+import { engine } from 'express-handlebars'
+import path  from 'path'
+import router from './route'
 
 // init
 const app = express()
 
 // setiing
-app.set('port', 3000)
+app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname, 'views') )
+app.engine('.hbs', engine ({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./lib/helpers')
+}))
+app.set('view engine', '.hbs')
 
 // middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // routes
+app.use('/books', router)
 
 // static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // starting server
 app.listen(app.get('port'), () => {
---
+++ src/route/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import indexController from '../controller'
+
+
+const router: Router = Router()
+
+router.get('/', indexController.index)
+
+export default router
